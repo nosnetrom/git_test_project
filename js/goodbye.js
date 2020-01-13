a0b46c533a9a15f58ff1f7b28df01eaaece010a6
Jim Mortenson <jmortenson@dealersocket.com>
2020-01-13 11:55:09 -0600

    Commit after first coding

--- js/goodbye.js
+++ js/goodbye.js
@@ -1,1 +1,10 @@
-document.write('Goodbye!');
+class Goodbye extends Component{
+    render() {
+        let now = new Date();
+        return (
+            <span>Goodbye at {now}!</span>
+        )
+    }
+}
+
+export default Goodbye;
--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,9 @@
-document.write('Hello!');
+class Hello extends Component{
+    render() {
+        let now = new Date();
+        return(
+            <span>Hello at {now}!</span>
+        )
+    }
+}
+export default Hello;
